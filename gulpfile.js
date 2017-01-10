e5e1a051fce35087943d1f353c47c68af6ec573b
L6zt <824561745@qq.com>
2017-01-10 17:42:02 +0800

    add gulp-connect to refresh page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,23 @@
 /**
  * Created by Administrator on 2017/1/10 0010.
  */
+var gulp=require('gulp'),
+    connect=require('gulp-connect')
+gulp.task('connect',function () {
+     connect.server({
+         livereload:true
+         });
+})
+gulp.task('html',function () {
+    gulp.src('*.html')               //todo: ./*.html not effective
+        .pipe(connect.reload())
+})
+
+gulp.task('watch',function () {
+    gulp.src('*.html')
+        .pipe(connect.reload())
+})
+gulp.task('watch',function () {
+    gulp.watch(['*.html'],['html']);
+})
+gulp.task('default',['connect','watch']);
